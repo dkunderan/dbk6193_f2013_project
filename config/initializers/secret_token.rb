c3cd1d8aa68ddb576d409540618826be5fa005d9
Deepthi Kunderan <diksha@Deepthis-MacBook-Pro.local>
2013-09-20 14:41:23 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '33f1d4a29843b30e013aea74f4ee5b5dad96319c28b566091f2ccf939cc58f30aea6aa5488a329e030676037e85d5e41246bcc77939cab7b386aa42d29192714'
---
+++ test/functional/products_controller_test.rb
@@ -0,0 +1,65 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionController::TestCase
+  setup do
+    @product = products(:one)
+    @update = {
+      :title       => 'Lorem Ipsum',
+      :description => 'Wibbles are fun!',
+      :image_url   => 'lorem.jpg',
+      :price       => 19.95
+    }
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:products)
+    assert_select 'h3', 'Programming Ruby 1.9'
+    assert_select '.list_actions' do |elements|
+      elements.each do |element|
+       assert_select element, "a", 3
+      end
+     end
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create product" do
+    assert_difference('Product.count') do
+      post :create, :product => @update
+    end
+
+    assert_redirected_to product_path(assigns(:product))
+  end
+
+  # ...
+
+  test "should show product" do
+    get :show, :id => @product.to_param
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, :id => @product.to_param
+    assert_response :success
+  end
+  
+  test "should update product" do
+    put :update, :id => @product.to_param, :product => @update
+    assert_redirected_to product_path(assigns(:product))
+  end
+
+  # ...
+
+  test "should destroy product" do
+    assert_difference('Product.count', -1) do
+      delete :destroy, :id => @product.to_param
+    end
+
+    assert_redirected_to products_path
+  end
+end
