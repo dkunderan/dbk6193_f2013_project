6a151a1cf1c6d678e198663fddd78f6b9a1bd03a
Deepthi Kunderan <diksha@Deepthis-MacBook-Pro.local>
2013-10-02 10:09:04 -0400

    Third commit

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,10 +1,14 @@
 class StoreController < ApplicationController
+  skip_before_filter :authorize
   def index
-    @products = Product.all
-    @cart = current_cart
-    @counter = session[:counter]
-    @counter.nil? ? @counter = 1 : @counter+=1
-    session[:counter] = @counter
+  if params[:set_locale]
+      redirect_to store_path(:locale => params[:set_locale])
+    else
+      @products = Product.all
+      @cart = current_cart
+      @counter = session[:counter]
+      @counter.nil? ? @counter = 1 : @counter+=1
+      session[:counter] = @counter
+    end
   end
-
 end
